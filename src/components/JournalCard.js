c495937c5a6fc6a12e247ce50d56eb349abd50ed
Riko <riko.fluchel@gmail.com>
2025-04-27 06:52:31 -0400

    add card styling

--- src/components/JournalCard.js
+++ src/components/JournalCard.js
@@ -2,14 +2,12 @@ import React from "react";
 
 function JournalCard({date, mood, description, duration, activity }) {
 
-    console.log(duration)
-
     const dateObj = new Date(date);
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = new Intl.DateTimeFormat('en-US', options).format(dateObj);
 
     return (
-        <div>
+        <div className="card">
             <h1>{formattedDate}</h1>
             <p>Mood: {mood}</p>
             <p>{activity}, {duration} minutes</p>
